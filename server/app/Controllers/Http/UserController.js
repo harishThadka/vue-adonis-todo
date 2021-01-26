c464773969ba50e30eefb9124f64d7f538d2264e
Harish Thadka <harish24680@gmail.com>
2021-01-25 20:02:26 -0600

    register api

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const User = use('App/Models/User');
+
+class UserController {
+
+    async register({ request }) {
+        const { email, password } = request.all();
+        console.log(email, password);
+        const user = await User.create({
+            email,
+            password,
+            username: email,
+        });
+        return user;
+    }
+
+}
+
+module.exports = UserController;
