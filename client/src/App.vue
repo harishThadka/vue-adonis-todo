a37f5bb58b791eede4baa582d71185d6c3a3452d
Harish Thadka <harish24680@gmail.com>
2021-01-26 14:50:50 -0600

    added dictionary

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <v-app>
+  <!-- <v-app> -->
+    <!-- <v-card color="grey lighten-4" flat height="200px" tile>
+      <v-toolbar dense>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card> -->
     <div id="app">
       <div id="nav">
         <router-link to="/">Home</router-link> |
@@ -7,7 +28,7 @@
       </div>
       <router-view />
     </div>
-  </v-app>
+  <!-- </v-app> -->
 </template>
 
 <style lang="scss">
